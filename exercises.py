cff75526c2c58958f3412c98d5492847e23e09bc
mroatmeal <jfeind101@gmail.com>
2024-10-07 13:20:57 -0500

    end of problem one

--- exercises.py
+++ exercises.py
@@ -1,5 +1,6 @@
 # Step One: Working in Python Files
-# For all the syntax challenges, do your work in script files (Python files normally end with .py). We’ve provided a starter file for each of these with the problem statement and simple print-style tests already.
+# For all the syntax challenges, do your work in script files (Python files normally end with .py). 
+# We’ve provided a starter file for each of these with the problem statement and simple print-style tests already.
 # count_up.py
 # in_range.py
 # sum.py
@@ -7,7 +8,9 @@
 # convert.py
 # If you need a reminder of how to convert between Farenheit and Celsius, feel free to Google this
 # You can run a Python script in one of two ways:
-# When you’re already in IPython, you can run a script with %run myscript.py. This runs your script and leaves you in IPython, where you can examine variables, run functions, etc, from your script. This is often the best way to try things out.
+# When you’re already in IPython, you can run a script with %run myscript.py. 
+# This runs your script and leaves you in IPython, where you can examine variables, run functions, etc, from your script. 
+# This is often the best way to try things out.
 # From the shell, you can run your script as python3 myscript.py
 # Step Two: Starting On Your Own
 
@@ -18,6 +21,7 @@ def count_up(start, stop):
         count_up(5, 7)
    should print: 5 6 7 """
     # YOUR CODE HERE
-    return range(start, stop, 1)
+    return list(range(start, stop+1, 1))
 
-count_up(5, 7)       
+print(count_up(5, 7))
+
