e069ebeda42fed127425c8f0912e60b17b9c9dd1
mroatmeal <jfeind101@gmail.com>
2024-10-09 08:48:11 -0500

    start of exercise seven

--- exercises.py
+++ exercises.py
@@ -108,17 +108,19 @@
 # Do this work in a new file, words.py.
 # 1. For a list of words, print out each word on a separate line, but in all uppercase. 
 # How can you change a word to uppercase? Ask Python for help on what you can do with strings!
+# 2. Turn that into a function, print_upper_words. Test it out. (Don’t forget to add a docstring to your function!)
+# def print_upper_words(words_in):
+#     for words in words_in:
+#         print (words.upper())
+
+# print_upper_words(["hello", "hey", "goodbye", "yo", "yes"])
+
+# 3. Change that function so that it only prints words that start with the letter ‘e’ (either upper or lowercase).
 def print_upper_words(words_in):
     for words in words_in:
         print (words.upper())
 
 print_upper_words(["hello", "hey", "goodbye", "yo", "yes"])
-
-# 2. Turn that into a function, print_upper_words. Test it out. (Don’t forget to add a docstring to your function!)
-
-
-# 3. Change that function so that it only prints words that start with the letter ‘e’ (either upper or lowercase).
-
 
 # 4. Make your function more general: you should be able to pass in a set of letters, and it only prints words that 
 # start with one of those letters. For example: this should print "HELLO", "HEY", "YO", and "YES"
